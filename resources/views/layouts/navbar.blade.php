b498e7d30150d9fecdb7633fd9c24d660dee3624
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2023-05-06 18:50:40 +0300

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('navbar')
+<ul class="navbar-nav">
+    <li class="nav-item"><a class="nav-link " href="login">Login</a></li>
+    <li class="nav-item"><a class="nav-link " href="signup">Signup</a></li>
+
+    <li class="nav-item"><a class="nav-link" href="dashboard">Dashboard</a></li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <ion-icon class="ion-icon ion-nav-icon" name="person-circle-outline"></ion-icon>
+            Username
+        </a>
+        <ul class="dropdown-menu">
+            <li><a href="account" class="dropdown-item" href="#">
+                    <ion-icon class="ion-icon drop-down-icon" name="settings"></ion-icon> Account
+                </a></li>
+            <li class="nav-item"> <a class="dropdown-item logout-btn" href="./controllers/auth_controller.php?q=logout"><ion-icon class="ion-icon drop-down-icon" name="log-out-outline"></ion-icon>Logout</a></li>
+        </ul>
+    </li>
+</ul>
+@endsection
